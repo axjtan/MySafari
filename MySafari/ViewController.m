cd1ba89b60cb55dadbed7b1b6f3ca4223677fd24
NP ECE BME Centre <BMEAdmin@NPs-MacBook-Air.gateway.2wire.net>
2015-07-17 23:04:43 +0800

    Second commit

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
 @end
 
@@ -17,6 +19,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void) webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void) webViewDidFinishLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString *urlString = [@"http://" stringByAppendingString: textField.text];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    
+    
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (IBAction)onBackButtonPressed:(UIButton *)sender {
+    if ([self.webView canGoBack]) {
+        [self.webView goBack];
+    }
+}
+
+- (IBAction)onForwardButtonPressed:(UIButton *)sender {
+    if ([self.webView canGoForward]) {
+        [self.webView goForward];
+    }
+}
+
+- (IBAction)onStopLoadingButtonPressed:(UIButton *)sender {
+    if (self.webView && self.webView.loading){
+        [self.webView stopLoading];
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    }
+}
+
+- (IBAction)onReloadButtonPressed:(UIButton *)sender {
+    if (self.webView && self.webView.loading) {
+        [self.webView reload];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
